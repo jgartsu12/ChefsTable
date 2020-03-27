e0b37a03c781c486593537f490b1e17374ed588d
jgartsu12 <jgartsu12@gmail.com>
2020-03-27 13:54:55 -0400

    added style sheets and fonts

---
+++ src/components/App/MainLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Header from '../headernavbarfooter';
+import Navbar from '../headernavbarfooter/navbar';
+
+export default class MainLayout from Component {
+    render() {
+        return (
+            <div className='main-layout-wrapper'>
+                
+            </div>
+        );
+    }
+}
