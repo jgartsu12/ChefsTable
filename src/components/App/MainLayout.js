2da5313caf4fb4e559caa26f7372c491a0fdcb2f
jgartsu12 <jgartsu12@gmail.com>
2020-03-28 18:38:03 -0400

    started creating home and aboout page

--- src/components/App/MainLayout.js
+++ src/components/App/MainLayout.js
@@ -5,6 +5,7 @@ import Navbar from '../headernavbarfooter/navbar';
 import PageTitle from '../pageTitle';
 import SocialMediaFooter from '../headernavbarfooter/socialMediaFooter';
 
+import StoreFrontImage from '../../../static/assets/images/restaurant-bldg-image.jpg';
 
 export default class MainLayout extends Component {
     render() {
@@ -12,8 +13,24 @@ export default class MainLayout extends Component {
             <div className='main-layout-wrapper'>
                 <Header/>
                 <Navbar/>
+                            {/* home page */}
                     <div className='homepage-wrapper'>
                         <PageTitle className='home-page' title='Welcome!'/>
+                        <div className='home-page-content'>
+                            <div className='left-column'>
+                                <img src={StoreFrontImage}/>
+                            </div>
+                            <div className='right-column'>
+                
+                            </div>
+                        </div>
+                    </div>
+                            {/* About Us page */}
+                    <div className='about-us-page-wrapper'>
+                        <PageTitle className='about-us-page' title='About Us'/>
+                        <div className='about-us-page-content'>
+                            {/* add rich bread img */}
+                        </div>
                     </div>
                 <SocialMediaFooter/>
             </div>
