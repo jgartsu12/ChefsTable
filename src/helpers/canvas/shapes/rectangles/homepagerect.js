66eeacea8d37a3bf9710d06fa1d34f15cfedc312
jgartsu12 <jgartsu12@gmail.com>
2020-03-28 18:50:23 -0400

    installed react-konva

---
+++ src/helpers/canvas/shapes/rectangles/homepagerect.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+export default class HomePageRectangle extends Component {
+    render() {
+        return (
+
+        );
+    }
+}
