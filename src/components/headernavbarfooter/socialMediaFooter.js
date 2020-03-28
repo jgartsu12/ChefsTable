1bbdb41669141fe8255812cff99836421c2c2d32
jgartsu12 <jgartsu12@gmail.com>
2020-03-28 09:03:16 -0400

    installed font-awesome, starting to create social media footer

---
+++ src/components/headernavbarfooter/socialMediaFooter.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SocialMediaFooter extends Component {
+    render() {
+        return (
+            <div className='social-media-footer-wrapper'>
+                
+            </div>
+        );
+    }
+}
+
+export default SocialMediaFooter;
