829ce975d213d9f1a9e8c864e452acdcd13673ba
jgartsu12 <jgartsu12@gmail.com>
2020-03-28 19:25:27 -0400

    added text to rectangle

--- src/helpers/canvas/shapes/rectangles/homepagerect.js
+++ src/helpers/canvas/shapes/rectangles/homepagerect.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import { Stage, Layer, Rect, Text } from 'react-konva';
-import Konva from 'konva';
 
 export default class HomePageRectangle extends Component {
+    
+    state = {
+        color: 'white'
+    };
+
     render() {
         return (
+            <div className='white-rectangle'>
+                <Rect
+                    x={200}
+                    y={200}
+                    width={200}
+                    height={200}
+                    fill={this.state.color}
+                /> 
 
+                <Stage width={window.innerWidth} height={window.innerHeight}>
+                    <Layer>
+                        <Text text='Open Monday-Saturday: 7AM - 9PM & Sunday: 7AM-8PM'/>
+                    </Layer>
+                </Stage>  
+            </div>
         );
     }
 }
