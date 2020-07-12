41f28e08f3cccd1bf915baf0f428e214b3be629a
jgartsu12 <jgartsu12@gmail.com>
2020-07-11 21:27:08 -0400

    debugging Failed to load resource: the server responded with a status of 500 (INTERNAL SERVER ERROR) getPhlogItem failed Error: Request failed with status code 500

--- src/components/phlog-mark-II/PhlogManager.js
+++ src/components/phlog-mark-II/PhlogManager.js
@@ -44,8 +44,8 @@ const PhlogManager = () => {
                     title: title,
                     // genre: genre,
                     // Either http or https
-                    image_url: cloudinary.data.url,
-                    // image_url: cloudinary.data.secure_url,
+                    // image_url: cloudinary.data.url,
+                    image_url: cloudinary.data.secure_url,
                     public_id: cloudinary.data.public_id
                 },
                 headers: {
